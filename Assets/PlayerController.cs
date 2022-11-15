e8a95c094175820a3211fa9959a12d5a7c2877bb
S♡PHIE <ssimpson2022@elam.co.uk>
2022-11-15 12:39:52 +0000

    Updated player controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
     float rotation = 0.0f;
-    public   float maxSpeed = 1.0f;
+    public   float speed = 1.0f;
     float camRotation = 0.0f;
    public  float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     GameObject cam;
     Rigidbody myRigidBody;
 
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
+    bool doubleJump = true;
+
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
         cam = GameObject.Find("Main Camera");
         myRigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
         //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
         //transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * speed) + (transform.right * Input.GetAxis("Horizontal") * speed);
         myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
 
 
@@ -32,5 +46,36 @@ public class PlayerController : MonoBehaviour
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40f);
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = normalSpeed;
+        }
+
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            doubleJump = true;
+            myRigidBody.AddForce(transform.up * jumpForce);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space) && !isOnGround)
+        {
+            if (doubleJump == true)
+            {
+                 myRigidBody.AddForce(transform.up * jumpForce);
+                 doubleJump = false;
+            }
+        }
+
+
     }
+
+   
 }
