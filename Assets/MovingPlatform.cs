4e5f0e5c6567bc528d4628a85e697722469f81a9
S♡PHIE <ssimpson2022@elam.co.uk>
2022-12-01 12:43:18 +0000

    MOVINHG PLAYTFORM

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -8,42 +8,35 @@ public class MovingPlatform : MonoBehaviour
     public Transform myPlatform;
     public Transform myStartPoint;
     public Transform myEndPoint;
-
-    public float platformSpeed;
+    public float speed;
     bool isReversing = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
         myPlatform.position = myStartPoint.position;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if (isReversing == false)
         {
-           myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, platformSpeed);
-         
-        }
-        else
+             myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, speed);
+
+             if (myPlatform.position == myEndPoint.position)
+             {
+                 isReversing = true;
+             }
+        } else
         {
-             myPlatform.position = Vector3.MoveTowards(myPlatform.position, myStartPoint.position, platformSpeed);
-             if (myPlatform.position == myStartPoint.position);
-             {
-                 isReversing = false;
-             }
+            myPlatform.position = Vector3.MoveTowards(myPlatform.position, myStartPoint.position, speed);
+
+            if (myPlatform.position == myStartPoint.position)
+            {
+                isReversing = false;
+            }
         }
-          if (myPlatform.position == myEndPoint.position)
-           {
-               isReversing = true;
-           }
-        if (myPlatform.position == myStartPoint.position);
-             {
-                 isReversing = false;
-             }
+        
     }
 }
